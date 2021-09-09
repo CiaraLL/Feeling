9688da5f23ea10538d170b6c73958ceb71483d54
BeggarLi <1559067870@qq.com>
2021-09-10 01:02:23 +0800

    module分层

---
+++ feeling/src/main/java/com/li/feeling/welcome/WelcomeActivity.java
@@ -0,0 +1,11 @@
+package com.li.feeling.welcome;
+
+import com.li.fragment.base_page.activity.BaseActivity;
+
+/**
+ * author: lanweihua
+ * created on: 9/10/21 12:27 AM
+ * description: 启动activity
+ */
+public class WelcomeActivity extends BaseActivity {
+}
---
+++ framework/ui/base-page/src/main/java/com/li/fragment/base_page/activity/BaseActivity.java
@@ -0,0 +1,15 @@
+package com.li.fragment.base_page.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+/**
+ * author: lanweihua
+ * created on: 9/10/21 12:24 AM
+ * description: activity基类
+ * 作用：
+ * 1.
+ * 2.
+ * 3.
+ */
+public abstract class BaseActivity extends AppCompatActivity {
+}
