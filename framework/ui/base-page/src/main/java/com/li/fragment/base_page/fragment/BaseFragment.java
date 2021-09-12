9d296b75cfdad4f5cc9223f21b61b32530801d71
BeggarLi <1559067870@qq.com>
2021-09-12 23:40:11 +0800

    添加颜色资源的library

---
+++ framework/ui/base-page/src/main/java/com/li/fragment/base_page/fragment/BaseFragment.java
@@ -0,0 +1,9 @@
+package com.li.fragment.base_page.fragment;
+
+import androidx.fragment.app.Fragment;
+
+/**
+ * description: fragment基类
+ */
+public abstract class BaseFragment extends Fragment {
+}
