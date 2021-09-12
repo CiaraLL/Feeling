45bdc1804480cc2b22d4010b98aee565deb2ce2b
BeggarLi <1559067870@qq.com>
2021-09-12 18:20:12 +0800

    添加登陆module

---
+++ feeling/login/src/main/java/com/li/feeling/login/LoginActivity.java
@@ -0,0 +1,19 @@
+package com.li.feeling.login;
+
+import android.os.Bundle;
+
+import com.li.fragment.base_page.activity.BaseActivity;
+import com.lwh.login.R;
+
+/**
+ * description: 登陆的activity
+ */
+public class LoginActivity extends BaseActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_login_layout);
+  }
+
+}
---
+++ feeling/login/src/main/java/com/li/feeling/register/RegisterActivity.java
@@ -0,0 +1,12 @@
+package com.li.feeling.register;
+
+import com.li.fragment.base_page.activity.BaseActivity;
+
+/**
+ * description:
+ */
+public class RegisterActivity extends BaseActivity {
+
+
+
+}
