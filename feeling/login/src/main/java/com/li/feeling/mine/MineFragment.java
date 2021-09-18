fc55f451e0356fa5bfcee51cfa904db092a61f09
BeggarLi <1559067870@qq.com>
2021-09-18 15:40:21 +0800

    完善MineFragment

---
+++ feeling/login/src/main/java/com/li/feeling/mine/MineFragment.java
@@ -0,0 +1,38 @@
+package com.li.feeling.mine;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.li.feeling.R;
+import com.li.fragment.base_page.fragment.BaseFragment;
+
+/**
+ * mine页面的fragment
+ */
+
+public class MineFragment extends BaseFragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_mine_layout, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        initView();
+    }
+
+    private void initView() {
+
+    }
+}
