bff6eeeb545c2e6aa59fd1af646492db934bbdef
BeggarLi <1559067870@qq.com>
2021-09-27 17:48:34 +0800

    Feel 序列化名字

--- feeling/model/src/main/java/com/li/feeling/model/Feel.java
+++ feeling/model/src/main/java/com/li/feeling/model/Feel.java
@@ -1,5 +1,7 @@
 package com.li.feeling.model;
 
+import com.google.gson.annotations.SerializedName;
+
 import java.io.Serializable;
 
 /**
@@ -7,15 +9,20 @@ import java.io.Serializable;
  */
 public class Feel implements Serializable {
 
+  @SerializedName("id")
   public long mId;
   // 所属者
+  @SerializedName("user")
   public User mUser;
   // 发布时间
+  @SerializedName("publishTime")
   public long mPublishTime;
   // 内容文案
+  @SerializedName("contentText")
   public String mContentText;
 
   // 点赞数
+  @SerializedName("likeNum")
   public int mLikeNum;
 
   public Feel() {
