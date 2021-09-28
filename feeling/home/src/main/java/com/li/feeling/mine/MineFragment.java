930911852a21a896c548f2173d5bee78ef7e46f8
BeggarLi <1559067870@qq.com>
2021-09-28 21:26:28 +0800

    添加个人资料页面

--- feeling/home/src/main/java/com/li/feeling/mine/MineFragment.java
+++ feeling/home/src/main/java/com/li/feeling/mine/MineFragment.java
@@ -4,13 +4,21 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 import com.li.feeling.home.R;
+import com.li.feeling.model.CurrentUser;
 import com.li.fragment.base_page.fragment.BaseFragment;
+import com.li.framework.ui.utility.DuplicatedClickFilter;
 
 public class MineFragment extends BaseFragment {
+  private TextView mUserName;
+  private TextView mBriefIntroduction;
+  private LinearLayout mMyView;
 
   @Override
   public View onCreateView(
@@ -23,10 +31,22 @@ public class MineFragment extends BaseFragment {
   @Override
   public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
     super.onViewCreated(view, savedInstanceState);
-    initView();
+    initView(view);
   }
 
-  private void initView() {
+  private void initView(View view) {
+    mUserName = view.findViewById(R.id.mine_content_user_name);
+    mBriefIntroduction = view.findViewById(R.id.mine_head_brief_introduction_view);
+    mMyView = view.findViewById(R.id.my_home_pager);
 
+    mUserName.setText(CurrentUser.get().getNickName());
+    mBriefIntroduction.setText(CurrentUser.get().getSex());
+    mMyView.setOnClickListener(new DuplicatedClickFilter(){
+
+      @Override
+      protected void handleClickEvent() {
+        MyHomePagerActivity.start(getActivity());
+      }
+    });
   }
 }
