d309866bdecc807035c6c222c50117ba7fd4e5d9
BeggarLi <1559067870@qq.com>
2021-09-29 15:44:43 +0800

    给PublishFeelActivity添加PublishFeelFragment

--- feeling/publish/src/main/java/com/li/feeling/publish/PublishFeelActivity.java
+++ feeling/publish/src/main/java/com/li/feeling/publish/PublishFeelActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
 
 import com.li.fragment.base_page.activity.BaseActivity;
 
@@ -15,15 +16,26 @@ import com.li.fragment.base_page.activity.BaseActivity;
 // TODO: 2021/9/28 加一个fragment
 public class PublishFeelActivity extends BaseActivity {
 
-    public static void start(@NonNull Activity activity){
-        Intent intent = new Intent(activity,PublishFeelActivity.class);
-        activity.startActivity(intent);
-    }
+  public static void start(@NonNull Activity activity) {
+    Intent intent = new Intent(activity, PublishFeelActivity.class);
+    activity.startActivity(intent);
+  }
 
-    @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_publish_feel_layout);
-    }
+  @Override
+  protected void onCreate(@Nullable Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    replacePublishFragment();
+  }
+
+  private void replacePublishFragment() {
+    getSupportFragmentManager()
+        .beginTransaction()
+        .replace(android.R.id.content, getPublishFragment())
+        .commitAllowingStateLoss();
+  }
+
+  private Fragment getPublishFragment() {
+    return new PublishFeelFragment();
+  }
 
 }
---
+++ feeling/publish/src/main/java/com/li/feeling/publish/PublishFeelFragment.java
@@ -0,0 +1,58 @@
+package com.li.feeling.publish;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.li.feeling.home.HomeActivity;
+import com.li.fragment.base_page.fragment.BaseFragment;
+import com.li.framework.ui.utility.DuplicatedClickFilter;
+
+public class PublishFeelFragment extends BaseFragment {
+
+  private TextView mCancelView;
+  private Button mButton;
+
+  @Nullable
+  @Override
+  public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+      @Nullable Bundle savedInstanceState) {
+    return inflater.inflate(R.layout.fragment_publish_feel_layout, container, false);
+  }
+
+  @Override
+  public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+    super.onViewCreated(view, savedInstanceState);
+    initView(view);
+  }
+
+  private void initView(View view) {
+    mCancelView = view.findViewById(R.id.publish_feel_page_cancel_view);
+    mButton = view.findViewById(R.id.publish_feel_page_publish_view);
+
+    mCancelView.setOnClickListener(new DuplicatedClickFilter() {
+      @Override
+      protected void handleClickEvent() {
+        Activity activity = getActivity();
+        if (activity != null) {
+          HomeActivity.start(activity);
+          activity.finish();
+        }
+      }
+    });
+
+    mButton.setOnClickListener(new DuplicatedClickFilter() {
+
+      @Override
+      protected void handleClickEvent() {
+
+      }
+    });
+  }
+}
